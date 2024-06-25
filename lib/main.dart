5b1e7383363c3e0233a42c45bd745987dfbc896e
Umer Ahmed <meru5350@gmail.com>
2024-06-25 16:52:20 +0500

    v1.0

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'gradient_container.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: GradientContainer(
+           Color.fromARGB(255, 42, 4, 88),
+           Color.fromARGB(255, 202, 100, 225),
+        ),
+      ),
+    ),
+  );
+}
